fa54f7d599805d9f0179c88ed83dc0a46cdfcc97
blackpig <blackpig5210@gmail.com>
2016-08-06 18:52:43 +0800

    加入显示天气功能

--- app/src/main/java/com/coolweather/app/util/HttpUtil.java
+++ app/src/main/java/com/coolweather/app/util/HttpUtil.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
@@ -20,8 +19,8 @@ public class HttpUtil {
                     URL url = new URL(address);
                     connection = (HttpURLConnection) url.openConnection();
                     connection.setRequestMethod("GET");
-                    connection.setConnectTimeout(10000);
-                    connection.setReadTimeout(10000);
+                    connection.setConnectTimeout(5000);
+                    connection.setReadTimeout(5000);
                     InputStream in = connection.getInputStream();
                     BufferedReader reader = new BufferedReader(new InputStreamReader(in));
                     StringBuilder response = new StringBuilder();
